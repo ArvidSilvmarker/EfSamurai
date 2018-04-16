8789a13cf28de852c90071cc8a244095cdb4cde0
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-04-16 13:09:13 +0200

    Added Name property

--- EfSamurai.Data/SamuraiContext.cs
+++ EfSamurai.Data/SamuraiContext.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EfSamurai.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace EfSamurai.Data
 {
-    class SamuraiContext
+    public class SamuraiContext : DbContext
     {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\mssqllocaldb; Database = EfSamurai; Trusted_Connection = True; ");
+        }
     }
 }
